f201768680274e974bd8fbb4ec486419e3b976c5
EC2 Default User <poseidon5631@gmail.com>
2018-09-06 12:07:19 +0000

    have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -13,8 +13,14 @@ class OwnershipsController < ApplicationController
     current_user.want(@item)
     flash[:success] = '商品を Want しました'
   end
+  if params[:type] == 'Have'
+    current_user.have(@item)
+    flash[:success] = '商品を Have しました'
+  end
+  
+  
   redirect_back(fallback_location: root_path)
-end
+  end
   
   def destroy
     @item = Item.find(params[:item_id])
@@ -23,6 +29,11 @@ end
      current_user.unwant(@item)
      flash[:success] = '商品の Want を解除しました'
   end
+  if params[:type]  == 'Have'
+     current_user.unhave(@item)
+     flash[:success] = '商品の Have を解除しました'
+  end
+  
   redirect_back(fallback_location: root_path)
   end
 end
