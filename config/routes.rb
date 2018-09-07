e596e79c8344790e52691e1820f0b3ae2d737619
EC2 Default User <poseidon5631@gmail.com>
2018-09-07 04:33:05 +0000

    ranking

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
-  
+  get 'rankings/want', to: 'rankings#want'
+  get 'rankings/have', to: 'rankings#have'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
